6919d6922384a1d979a5736e0d4983a1fb717878
ec5ug <ec5ug@virginia.edu>
2023-05-16 20:38:38 -0400

    UserInfo shows up on admin site

--- oauth_app/admin.py
+++ oauth_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from oauth_app.models import UserInfo
 
 # Register your models here.
+# class UserInfoInline(admin.StackedInline):
+#     model = UserInfo
+#     can_delete = False
+#     verbose_name_plural = 'UserInfo'
+#
+# class UserAdmin(BaseUserAdmin):
+#     inlines = [UserInfoInline]
+#
+# admin.site.unregister(User)
+# # admin.site.register(User, UserAdmin)
+# admin.site.register(User)
+admin.site.register(UserInfo)
--- oauth_app/models.py
+++ oauth_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, default="student", choices=[("student", "student"), ("school_admin", "school_admin")])
+    filled_form = models.BooleanField(default=False)
+    date_of_update = models.DateField(null=True)
+    age = models.PositiveIntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(123)])
+    edu_lvl = models.CharField(null=True, max_length=20)
+    state = models.CharField(null=True, max_length=14)
+    county = models.CharField(null=True, max_length=30)
+    school = models.CharField(null=True, max_length=50)
+    gpa = models.FloatField(null=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    classes = models.CharField(max_length=100)
+    intended_major = models.CharField(max_length=20)
+    interests = models.CharField(max_length=100)
