6aeb251b8493e4e5a326dc973780c3a27fa261f7
ec5ug <ec5ug@virginia.edu>
2023-05-17 20:35:29 -0400

    student form is set up

--- minerva/views.py
+++ minerva/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from oauth_app.models import UserInfo
-from oauth_app.forms import RegistrationForm
+from oauth_app.forms import Registration_Form, Student_Form
 from datetime import datetime
 
 # Create your views here.
@@ -9,6 +9,25 @@ def index(request):
         username = request.user
         if len(UserInfo.objects.filter(user=username)) == 0:
             current_date = datetime.today().date()
-            form = RegistrationForm()
-            return render(request, 'index.html', {'form': form})
+            registration_form = Registration_Form()
+            student_form = None
+            school_admin_form = None
+            if request.method == 'POST':
+                if ('register-submit' in request.POST):
+                    registration_form = Registration_Form(request.POST)
+                    if registration_form.is_valid():
+                        state = registration_form.cleaned_data['state']
+                        county = registration_form.cleaned_data['county']
+                        school = registration_form.cleaned_data['school']
+                        role = registration_form.cleaned_data['role']
+                        if role == 'student':
+                            student_form = Student_Form()
+                            print("I am a student")
+                        elif role == 'school_admin':
+                            print("I requested to be a school admin")
+                        else:
+                            print("Naming works")
+            return render(request, 'index.html',
+                          {'registration_form': registration_form, 'student_form':student_form,
+                           'school_admin_form':school_admin_form})
     return render(request, 'index.html')
--- oauth_app/forms.py
+++ oauth_app/forms.py
@@ -1,8 +1,26 @@
 from django import forms
+from django_select2.forms import Select2MultipleWidget
 
-class RegistrationForm(forms.Form):
+class Registration_Form(forms.Form):
     state = forms.CharField(label="My state of residence is")
     county = forms.CharField(label="My county of residence is")
     school = forms.CharField(label="I attend the following school")
     role = forms.CharField(label="I intend to use this scholarship service as a",
-                           widget=forms.Select(choices=[('Student','Student'),('School_Admin','School Administrator')]))
+                           widget=forms.Select(choices=[('student','Student'),('school_admin','School Administrator')]))
+
+class Student_Form(forms.Form):
+    age = forms.IntegerField(label="I am __ years old", min_value=1, max_value=123)
+    edu_lvl = forms.CharField(label="My current level of education is",
+                              widget=forms.Select(choices=[("middle_school", "Middle School"),
+                                                           ("high_school", "High School"),
+                                                           ("undergraduate", "Undergraduate"), ("graduate", "Graduate"),
+                                                           ("doctoral", "Doctoral")]))
+    gpa = forms.FloatField(label="My (unweighted) GPA is __", min_value=0, max_value=5)
+    CLASS_CHOICES = [("algebra", "Algebra"), ("geometry", "Geometry")]
+    classes = forms.MultipleChoiceField(label="I have taken these courses", choices=CLASS_CHOICES,
+                                        widget=forms.CheckboxSelectMultiple(attrs={'class': 'column-choices'}))
+    MAJOR_CHOICES = [("chemistry", "Chemistry"), ("computer_science", "Computer Science")]
+    major = forms.MultipleChoiceField(label="I intend to major or am majoring in: ", choices=MAJOR_CHOICES,
+                                      widget=forms.CheckboxSelectMultiple(attrs={'class': 'column-choices'}))
+    # major = forms.MultipleChoiceField(label="I intend to major or am majoring in: ", choices=MAJOR_CHOICES,
+    #                                   initial='0', widget=forms.SelectMultiple())
